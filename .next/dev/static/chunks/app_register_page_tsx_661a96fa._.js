c256d2bd1d7e19b665e100a0e766e65977f98502
Yasu <barutanseven@gmail.com>
2025-11-26 21:58:32 +0900

    fix: remove .next from git

--- .next/dev/static/chunks/app_register_page_tsx_661a96fa._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_b9e364b6._.js"
-],
-    source: "dynamic"
-});
--- .next/server/chunks/ssr/app_layout_tsx_271801d7._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[33290,a=>{"use strict";var b=a.i(7997);function c({children:a}){return(0,b.jsx)("html",{lang:"ja",children:(0,b.jsx)("body",{children:a})})}a.s(["default",()=>c,"metadata",0,{title:"My RAG App",description:"LINE Bot & Web Chat Application"}])}];
-
-//# sourceMappingURL=app_layout_tsx_271801d7._.js.map
